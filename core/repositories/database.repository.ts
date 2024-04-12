40cdf8067f4640239d6affd153d079b40ba3d72c
DaElias <davidcerchiaro8@gmail.com>
2024-04-12 15:22:43 +0200

    create

--- core/repositories/database.repository.ts
+++
@@ -1,8 +0,0 @@
-import User from "../entities/user.entity";
-
-export default interface DatabaseRepository {
-    getAllUser(): Promise<User[]>
-    getUserById(id: string): Promise<User>
-    createUser(user: User): Promise<User>
-    deleteUserById(id: string): Promise<void>
-}
